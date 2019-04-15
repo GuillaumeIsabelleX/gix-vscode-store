fc5fa1493e2af2a588ca597363fa977dcf84d5e8
Guillaume Isabelle <jgi@jgwill.com>
2019-04-15 17:11:12 -0400

    GIX version * Default overwritting variable with config

--- src/xstore.ts
+++ src/xstore.ts
@@ -4,14 +4,18 @@ import * as clipboard from 'clipboardy';
 import * as fs from 'fs';
 import * as path from 'path';
 
+var bypassoverwrites = false;
 // get the store file path
-function getStoreFile() { 
+function getStoreFile() {
     // get from config 
-    var storePath: string = vscode.workspace.getConfiguration().get('store.path');
+    var storePath: string = vscode.workspace.getConfiguration().get('xstore.path');
+    var storeOverwriteVar: boolean = vscode.workspace.getConfiguration().get('xstore.bypassoverwrites');
+    bypassoverwrites = storeOverwriteVar;
+
     if (!storePath) {
         storePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
     }
-    const storeFilePath = path.join(storePath, '.vscode-store.json');
+    const storeFilePath = path.join(storePath, '.vscode-xstore.json');
     if (!fs.existsSync(storeFilePath)) {
         fs.writeFileSync(storeFilePath, '{}');
     };
@@ -31,17 +35,17 @@ function writeStore(data) {
 export const activate = (context: vscode.ExtensionContext) => {
 
     // Open store file
-    const openStore = vscode.commands.registerCommand('store.open', () => {
+    const openStore = vscode.commands.registerCommand('xstore.open', () => {
         const storePath = getStoreFile();
         vscode.workspace.openTextDocument(vscode.Uri.file(storePath))
-        .then((doc) => {
-            vscode.window.showTextDocument(doc);
-        })
+            .then((doc) => {
+                vscode.window.showTextDocument(doc);
+            })
 
     });
 
     // Create new store
-    const setStore = vscode.commands.registerCommand('store.set', () => {
+    const setStore = vscode.commands.registerCommand('xstore.set', () => {
         vscode.window.showInputBox({
             placeHolder: "Enter new key",
             ignoreFocusOut: true,
@@ -52,10 +56,11 @@ export const activate = (context: vscode.ExtensionContext) => {
             }
             const storeData = readStore();
             // warn the user if the key exists.
-            if (new Set(Object.keys(storeData)).has(key.trim())) {
-                vscode.window.showWarningMessage('Key already exists.')
-                return;
-            }
+            if (bypassoverwrites)
+                if (new Set(Object.keys(storeData)).has(key.trim())) {
+                    vscode.window.showWarningMessage('Key already exists.')
+                    return;
+                }
             const editor = vscode.window.activeTextEditor;
             vscode.window.showInputBox({
                 ignoreFocusOut: true,
@@ -77,7 +82,7 @@ export const activate = (context: vscode.ExtensionContext) => {
     context.subscriptions.push(setStore);
 
     // Get data from store
-    const getStore = vscode.commands.registerCommand('store.get', () => {
+    const getStore = vscode.commands.registerCommand('xstore.get', () => {
         // read the json from disk
         const storeData = readStore();
         vscode.window.showQuickPick(Object.keys(storeData))
@@ -95,7 +100,7 @@ export const activate = (context: vscode.ExtensionContext) => {
     context.subscriptions.push(getStore);
 
     // Remove data from store
-    const removeStore = vscode.commands.registerCommand('store.remove', () => {
+    const removeStore = vscode.commands.registerCommand('xstore.remove', () => {
         const storeData = readStore();
         vscode.window.showQuickPick(Object.keys(storeData))
             .then(key => {
@@ -104,7 +109,7 @@ export const activate = (context: vscode.ExtensionContext) => {
                 }
                 delete storeData[key.trim()]   // delete the key value
                 writeStore(storeData);  // write to store
-                vscode.window.showInformationMessage(`Removed '${key}' from store`);
+                vscode.window.showInformationMessage(`Removed '${key}' from xstore`);
             })
     })
 
